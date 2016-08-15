49178509ae1ac119619219fe79ca0925146acd8e
Gunnar Wolf <gwolf@gwolf.org>
2016-08-15 13:49:14 -0500

    Fix several Lintian warnings

--- debian/cron.d
+++ debian/cron.d
@@ -1,1 +1,1 @@
-0 * * * *    www-data          if test -x /usr/share/drupal8/scripts/cron.sh ; then /usr/share/drupal8/scripts/cron.sh ; fi
+0 * * * *    www-data          if test -x /usr/share/drupal8/core/scripts/cron.sh ; then /usr/share/drupal8/core/scripts/cron.sh ; fi
